0f956b2ca04cdb04125c7128de0f9607681c46ea
Anzumana Taal <anzumana.taal@me.com>
2019-02-25 13:11:45 +0100

    4: reorder a list with react-beautiful-dnd

--- src/column.jsx
+++ src/column.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
 import Task from './task';
 
 const Container = styled.div`
@@ -16,12 +17,22 @@ const TaskList = styled.div`
 `;
 
 export default class Column extends React.Component {
-	render(){
-		return <Container>
-			<Title> {this.props.column.title}</Title>
-			<TaskList> {this.props.tasks.map(task=> <Task key={task.id} task={task}/>)}
-			</TaskList>
-		</Container>
-		
+	render(){ 
+		return (
+			<Container>
+				<Title> {this.props.column.title}</Title>
+			<Droppable droppableId ={this.props.column.id}>
+			{(provided) => (
+				<TaskList 
+					ref={provided.innerRef}
+					{...provided.droppableProps}
+				> 
+					{this.props.tasks.map((task,index)=> <Task key={task.id} task={task} index ={index}/>)}
+					{provided.placeholder}
+				</TaskList>
+			)}
+			</Droppable>
+			</Container>
+		)
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import '@atlaskit/css-reset';
-
+import { DragDropContext } from 'react-beautiful-dnd';
 import initialData from './initial-data';
 import Column from './column';
 
+
 class App extends React.Component {
+	onDragEnd = result =>{
+		// TODO reoder our column
+	}
+		
 	state = initialData;
 	render(){
-		return  this.state.columnOrder.map(columnId => {
-			const column = this.state.columns[columnId];
-			const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
-			return <Column key={column.id} column={column} tasks={tasks} />
-		});
+		return (
+			<DragDropContext
+				onDragEnd= {this.onDragEnd}
+			>
+				{this.state.columnOrder.map(columnId => {
+					const column = this.state.columns[columnId];
+					const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+					return <Column key={column.id} column={column} tasks={tasks} />
+				})}
+			</DragDropContext>
+			);
 	}
 }
 ReactDOM.render(<App />, document.getElementById('root'));
