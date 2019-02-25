d416ae3e8953e2fb09f72de9450337ac28597a0b
Anzumana Taal <anzumana.taal@me.com>
2019-02-25 15:50:15 +0100

    8: designate control of dragging for a react-beautiful-dnd draggable with dragHandleProps

--- src/task.jsx
+++ src/task.jsx
@@ -9,8 +9,17 @@ const Container = styled.div`
 	padding: 8px;
 	margin-bottom: 8px;
 	background-color:${props => (props.isDragging? 'lightgreen' : 'white')};
+
+	display:flex
 `
 
+const Handle = styled.div`
+	width:20px;
+	height: 20px;
+	background-color:orange;
+	border-radius:4px;
+	margin-right:8px;
+`;
 export default class Task extends React.Component {
 	render(){
 		return  (
@@ -18,10 +27,12 @@ export default class Task extends React.Component {
 			{(provided,snapshot) => (
 				<Container 
 				{...provided.draggableProps}
-				{...provided.dragHandleProps}
 				ref={provided.innerRef}
 				isDragging={snapshot.isDragging}
-				> {this.props.task.content} </Container>
+				> 
+				<Handle {...provided.dragHandleProps} />
+				{this.props.task.content} 
+				</Container>
 			)}
 			</Draggable>
 		)
